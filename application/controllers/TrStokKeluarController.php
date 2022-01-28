bdfa8f3cb0070e85d2757e1b510dd8e76c9303a4
ACH RIZAL <achrizal15@gmail.com>
2022-01-29 02:24:14 +0700

    UPDATE

---
+++ application/controllers/TrStokKeluarController.php
@@ -0,0 +1,77 @@
+<?php
+
+
+class TrStokKeluarController extends CI_Controller
+{
+   public function __construct()
+   {
+      parent::__construct();
+      $this->load->library("main_libraries");
+      $this->load->library('session');
+      $this->load->model("distributormodels", "dm");
+      $this->load->model("trstokkeluarmodels", "tsm");
+      $this->load->model("produkmodels", "pm");
+   }
+   public function index()
+   {
+      $data["tsm"] = $this->tsm->get_all();
+      $this->main_libraries->innerview("tr_stok_masuk_view", $data);
+   }
+   public function action($params = "add", $id = "")
+   {
+      $data["aksi"] = strtolower($params);
+      $data["produk"] = $this->pm->get_all();
+      $data["distributor"] = $this->dm->get_all();
+      if ($params == "edit") {
+         $data["stokin"] = $this->tsm->get($id);
+         if (!$data["stokin"]) {
+            show_404();
+         }
+      }
+      $this->main_libraries->innerview("tr_stok_masuk_form", $data);
+   }
+   public function add()
+   {
+      $produk_id = $this->input->post("produk_id");
+      $data = [
+         "faktur" => $this->input->post("faktur"),
+         "produk_id" => $produk_id,
+         "distributor_id" => $this->input->post("distributor_id"),
+         "qty" => $this->input->post("qty"),
+         "catatan" => $this->input->post("catatan")
+      ];
+      $this->tsm->create($data);
+      $this->pm->perbarui($produk_id, ["qty" => $this->input->post("stok_awal")]);
+      $this->session->set_flashdata("message", "Data telah ditambahkan.");
+      redirect(base_url("trstokmasukcontroller"));
+   }
+   public function get_any($table, $id)
+   {
+      if ($table == "produk") {
+         echo json_encode($this->pm->get($id));
+      }
+   }
+   public function edit()
+   {
+      $produk_id = $this->input->post("produk_id");
+      $data = [
+         "faktur" => $this->input->post("faktur"),
+         "produk_id" => $produk_id,
+         "distributor_id" => $this->input->post("distributor_id"),
+         "qty" => $this->input->post("qty"),
+         "catatan" => $this->input->post("catatan")
+      ];
+      $this->tsm->perbarui($this->input->post("id"), $data);
+      $this->pm->perbarui($produk_id, ["qty" => $this->input->post("stok_awal")]);
+      $this->session->set_flashdata("message", "Data telah diperbarui.");
+      redirect(base_url("trstokmasukcontroller"));
+   }
+   public function delete()
+   {
+      $id = $this->input->post("id");
+      $stokmasuk=$this->tsm->get($id);
+      $produk=$this->pm->get($stokmasuk->trproduk_id);
+      $this->pm->perbarui($produk->id,["qty"=>$produk->qty-$stokmasuk->trqty]);
+      echo $this->tsm->delete($id);
+   }
+}
---
+++ application/models/TrStokKeluarmodels.php
@@ -0,0 +1,73 @@
+<?php
+
+class TrStokKeluarmodels extends CI_Model
+{
+   public function setAliasColumn($column, $alias)
+   {
+      $column =   $this->db->list_fields($column);
+      $result = [];
+      foreach ($column as $key) {
+         $result[] = $alias . "." . $key . " AS " . $alias . $key;
+      }
+      return $result;
+   }
+   public function get_all()
+   {
+      $table=[
+         ["table"=>"tr_stok_keluar","alias"=>"tr"],
+         ["table"=>"produk","alias"=>"p"],
+      ];
+      $select="";
+      foreach ($table as $key) {
+         $select .= implode(',', $this->setAliasColumn($key['table'], $key['alias'])) . ",";
+      }
+      $this->db->select($select);
+      $this->db->from("tr_stok_keluar tr");
+      $this->db->join("produk p","tr.produk_id=p.id");
+      $this->db->order_by("tr.created_at", "desc");
+      return $this->db->get()->result();
+   }
+
+   public function delete($id)
+   {
+      $this->db->delete("tr_stok_keluar", ["id" => $id]);
+      return $this->db->affected_rows();
+   }
+   public function get($id = "")
+   {
+      $table=[
+         ["table"=>"tr_stok_keluar","alias"=>"tr"],
+         ["table"=>"produk","alias"=>"p"],
+         ["table"=>"distributor","alias"=>"d"],
+      ];
+      $select="";
+      foreach ($table as $key) {
+         $select .= implode(',', $this->setAliasColumn($key['table'], $key['alias'])) . ",";
+      }
+      $this->db->select($select);
+      $this->db->from("tr_stok_keluar tr");
+      $this->db->join("produk p","tr.produk_id=p.id");
+      $this->db->join("distributor d","tr.distributor_id=d.id");
+      $this->db->where("tr.id", $id);
+      return $this->db->get()->row();
+   }
+   public function get_last()
+   {
+      $this->db->select("*");
+      $this->db->from("tr_stok_keluar");
+      $this->db->order_by("id", "desc");
+      return $this->db->get()->row();
+   }
+ 
+   public function perbarui($id, $data)
+   {
+      $this->db->update("tr_stok_keluar", $data, ["id" => $id]);
+      return $this->db->affected_rows();
+   }
+   public function create($data)
+   {
+      $data["created_at"] = date("Y-m-d", strtotime("now"));
+      $this->db->insert("tr_stok_keluar", $data);
+      return $this->db->affected_rows();
+   }
+}
